76c4e709ddc486afe67fc4d602c160866ef8b341
Jaime <jaimehiguerasmedina98@gmail.com>
2024-08-27 11:40:52 +0200

    deleted ai dependency

--- src/main/java/com/example/boe_auction/auction_web_scraping/ollama/controller/ChatController.java
+++ src/main/java/com/example/boe_auction/auction_web_scraping/ollama/controller/ChatController.java
@@ -2,7 +2,6 @@ package com.example.boe_auction.auction_web_scraping.ollama.controller;
 
 import com.example.boe_auction.auction_web_scraping.ollama.service.OllamaService;
 import lombok.AllArgsConstructor;
-import org.springframework.ai.ollama.OllamaChatModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,13 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 @AllArgsConstructor
 public class ChatController {
 
-    private final OllamaChatModel chatModel;
     private OllamaService ollamaService;
-
-    @GetMapping
-    public String chatWithOllama(String prompt) {
-        return chatModel.call(prompt);
-    }
 
     @GetMapping("/improve-address")
     public String improveAddressForGeoCoding(String prompt) {
