feb5c3f137b4ca5bc670b5d63645bf1d59e1c8b8
Jaime <jaimehiguerasmedina98@gmail.com>
2024-08-27 13:43:52 +0200

    now work!

--- src/main/java/com/example/boe_auction/auction_web_scraping/BoeAuctionApplication.java
+++ src/main/java/com/example/boe_auction/auction_web_scraping/BoeAuctionApplication.java
@@ -2,8 +2,10 @@ package com.example.boe_auction.auction_web_scraping;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
+@EnableMongoRepositories
 public class BoeAuctionApplication {
 
     public static void main(String[] args) {
