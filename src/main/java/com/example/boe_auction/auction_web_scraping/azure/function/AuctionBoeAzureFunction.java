b2b7a6943132deacc4363491a7776c1aa5fa4b34
HIGUERASME <jaime.higuerasmedina@dachser.com>
2024-09-06 10:23:19 +0200

    added logs and traces

--- src/main/java/com/example/boe_auction/auction_web_scraping/azure/function/AuctionBoeAzureFunction.java
+++ src/main/java/com/example/boe_auction/auction_web_scraping/azure/function/AuctionBoeAzureFunction.java
@@ -10,9 +10,6 @@ import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
-import java.net.URLDecoder;
-import java.nio.charset.StandardCharsets;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
